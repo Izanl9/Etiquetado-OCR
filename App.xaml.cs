d87eaa70f5a64fa9aa4d1c15cda344ed1e946a79
Izan Lecina <izanbot99@gmail.com>
2026-04-24 08:58:23 +0200

    Fix

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.Maui.Controls;
-
-namespace EtiquetadoAuto;
+﻿namespace EtiquetadoAuto;
 
 public partial class App : Application
 {
     public App()
     {
         InitializeComponent();
-
         MainPage = new AppShell();
     }
 }
