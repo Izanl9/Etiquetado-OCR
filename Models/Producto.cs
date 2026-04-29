a2da81089ca2d8354edf3cb1f1b71fc32e0443ca
izan <izanbot99@gmail.com>
2026-04-29 22:15:00 +0200

    Fix6

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,7 +1,13 @@
-namespace EtiquetadoAuto.Models;
-
-public class Producto
+namespace EtiquetadoAuto.Models
 {
-    public string Nombre { get; set; } = string.Empty;
-    public int Cantidad { get; set; }
+    public class Producto
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public int Cantidad { get; set; }
+        
+        // Añadimos estos para que Inventory.razor no de error
+        public string Codigo { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public DateTime LastUpdate { get; set; } = DateTime.Now;
+    }
 }
